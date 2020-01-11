76544d4ddb0820b79caa8a977d82de153dbce04a
Hasmuddin <mdaliansari33@gmail.com>
2020-01-11 12:16:28 +0530

    text added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <Navbar className="nav navbar">
+            <div className="nav-item">123</div>
+            <div className="nav-item">123</div>
+            <div className="nav-item">123</div>
+        </Navbar>
+    )
+}
